0d14df6a7bc4bbc8ef77d68365b0c4659289b3e9
Yousouph Olosan <depolymathyousouph@gmail.com>
2024-05-16 09:36:19 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//System.out.println("Hello world");
+      //  System.out.print("result is " + 5 + 7);
+           //   System.out.print("**");
+        //System.out.println();
+        //System.out.println("**");
+        //System.out.print("**");
+        System.out.print("1");
+        System.out.print("2");
+        System.out.println("3");
+        System.out.println();
+        System.out.println("4");
+        //System.out.println("XOX");
+//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        //System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
